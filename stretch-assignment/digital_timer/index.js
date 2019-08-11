0b7e5994fa48a4c0b1ed4b926af1f02a974e314a
Hector Valdez <hvaldez180@gmail.com>
2019-08-11 18:42:50 -0400

    working on making digital timer

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,3 +1,10 @@
+axios.get("/Users/hectorvaldez/Desktop/github/DOM-I/stretch-assignment/digital_timer/index.js")
+.then(response => {
+    console.log(response)
+})
+
+function creatingTimer() {
+
 //creating elements
 
     const divContainer = document.createElement("div");
@@ -6,6 +13,7 @@
     const divColon = document.createElement("div");
     const divmsHundreds = document.createElement("div");
     const divmsTens = document.createElement("div");
+    const btn = document.createElement("button");
 
 //structuring elements
 
@@ -14,6 +22,7 @@
     divContainer.appendChild(divColon)
     divColon.appendChild(divmsHundreds)
     divContainer.appendChild(divmsTens)
+    divContainer.appendChild(btn)
 
 //adding classes to elements
 
@@ -31,3 +40,13 @@
     divColon.setAttribute("id", "colon")
     divmsHundreds.setAttribute("id", "msHundreds")
     divmsTens.setAttribute("id", "msTens")
+
+//setting up timer
+
+    const tickingTimer = setInterval(creatingTimer, 10000);
+  
+    return divContainer;
+}
+
+const addingToDom = document.querySelector(".digits")
+addingToDom.appendChild(creatingTimer())
